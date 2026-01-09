915267aad1252946039bf73a4ee4313178a16636
Tiago Henrique <thenriqueleite277@gmail.com>
2026-01-09 10:23:27 -0300

    feat: update GaleriaDeFotos component to use new Photo type structure and improve photo rendering

--- galeria_de_fotos/src/components/galeriaDeFotos.tsx
+++ galeria_de_fotos/src/components/galeriaDeFotos.tsx
@@ -1,28 +1,39 @@
-type Photo = {
-  id: number;
-  title: string;
-  url: string;
+
+export type Photo = {
+  id: string;
+  alt_description: string | null;
+  urls: {
+    small: string;
+    regular: string;
+  };
 };
 
-export default function GaleriaDeFotos({ photos }: {photos: Photo[]}){
+type Props = {
+  photos: Photo[];
+};
+export default function GaleriaDeFotos({ photos }: Props){
     if (photos.length === 0) {
-      return (
-        <p className="text-center text-white mt-10">
-            Nenhuma foto encontrada
-        </p>
-        );
-    }
+    return (
+      <p className="text-center text-white mt-10">
+        Nenhuma foto encontrada
+      </p>
+    );
+  }
 
-    return(
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-[1200px] mx-auto p-5">
-            {photos.map(photo => (
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-[1200px] mx-auto p-5">
+      {photos.map(photo => (
         <div
           key={photo.id}
           className="bg-white rounded shadow hover:scale-105 transition"
         >
-          <img src={photo.url} alt={photo.title} className="rounded-t" />
-          <p className="text-center font-medium p-2">
-            {photo.title}
+          <img
+            src={photo.urls.small}
+            alt={photo.alt_description || "Foto"}
+            className="rounded-t"
+          />
+          <p className="text-center p-2 font-medium">
+            {photo.alt_description || "Sem título"}
           </p>
         </div>
       ))}
